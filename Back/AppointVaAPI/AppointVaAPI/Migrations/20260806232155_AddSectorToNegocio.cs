6efcb7a563da965e672da2db97c596124c532d00
Roberto Hilario Vasquez <hilario.roberto.1cm@outlook.com>
2026-08-06 17:25:19 -0600

    fix(migration): add CHECK constraint restricting Sector to belleza|salud

--- Back/AppointVaAPI/AppointVaAPI/Migrations/20260806232155_AddSectorToNegocio.cs
+++ Back/AppointVaAPI/AppointVaAPI/Migrations/20260806232155_AddSectorToNegocio.cs
@@ -17,11 +17,15 @@ namespace AppointVaAPI.Migrations
                 maxLength: 20,
                 nullable: false,
                 defaultValue: "belleza");
+
+            migrationBuilder.Sql("ALTER TABLE \"Negocios\" ADD CONSTRAINT \"CK_Negocios_Sector\" CHECK (\"Sector\" IN ('belleza', 'salud'));");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("ALTER TABLE \"Negocios\" DROP CONSTRAINT IF EXISTS \"CK_Negocios_Sector\";");
+
             migrationBuilder.DropColumn(
                 name: "Sector",
                 table: "Negocios");
