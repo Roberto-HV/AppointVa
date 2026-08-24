d83b549682cfc4b6e91dbe8644e75f512b1b85f6
Roberto Hilario Vasquez <hilario.roberto.1cm@outlook.com>
2026-08-24 12:29:05 -0600

    fix(booking): scale header height with screen width to reduce cover zoom distortion

--- Front/src/pages/publico/BookingPage.tsx
+++ Front/src/pages/publico/BookingPage.tsx
@@ -760,7 +760,7 @@ export default function BookingPage() {
     <div className="min-h-screen bg-slate-50">
       {/* Header oscuro del negocio */}
       <div
-        className="relative overflow-hidden min-h-[180px] sm:min-h-[260px] flex flex-col justify-end"
+        className="relative overflow-hidden min-h-[180px] sm:min-h-[260px] lg:min-h-[300px] xl:min-h-[340px] flex flex-col justify-end"
         style={{ background: "#0C0C0F" }}
       >
         {/* Portada visible */}
